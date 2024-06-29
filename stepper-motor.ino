f9ace183390fecb07da0a741b923c20fb3444ef9
SankalyaTharindi <165166262+SankalyaTharindi@users.noreply.github.com>
2024-06-29 15:03:44 +0530

    Update stepper-motor.ino

---
+++ stepper-motor.ino
@@ -0,0 +1,23 @@
+int stepPin = 2;      // Connect to the step pin of the driver
+int directionPin = 3; // Connect to the direction pin of the driver
+
+void setup() {
+  // Set the pins as outputs
+  pinMode(stepPin, OUTPUT);
+  pinMode(directionPin, OUTPUT);
+  
+  // Set the direction to clockwise
+  digitalWrite(directionPin, HIGH);
+
+  // Move the motor 4000 steps clockwise
+  for(long stepCount = 1; stepCount <= 20000; stepCount++) {  // Changed int to long
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(0.001);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  }
+}
+
+void loop() {
+  // Do nothing here, the code in setup() runs only once
+}
